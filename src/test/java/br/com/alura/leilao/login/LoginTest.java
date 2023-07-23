a6429d1811e5aae2f9e6b1614e9330bc69eb4836
Maria Eduarda <mduda_araujo@hotmail.com>
2023-07-23 19:00:50 -0300

    Login Test with invalid information and refactor

--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -1,2 +1,56 @@
-package br.com.alura.leilao.login;public class LoginTest {
+package br.com.alura.leilao.login;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    public static final String URL_LOGIN = "http://localhost:8080/login?error";
+    private WebDriver browser;
+
+    @BeforeAll
+    public static void BeforeAll() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void BeforeEach() {
+        this.browser = new ChromeDriver();
+        browser.navigate().to(URL_LOGIN);
+    }
+
+    @AfterEach
+    public void AfterEach() {
+        browser.quit();
+    }
+
+    @Test
+    public void DeveriaRealizarLoginComDadosValidos() {
+
+        browser.findElement(By.id("username")).sendKeys("fulano");
+        browser.findElement(By.id("password")).sendKeys("pass");
+        browser.findElement(By.id("login-form")).submit();
+
+        Assert.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+        Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
+
+    }
+
+    @Test
+    public void NaoDeveriaLogarComDadosInvalidos() {
+
+        browser.findElement(By.id("username")).sendKeys("userInvalido");
+        browser.findElement(By.id("password")).sendKeys("pass");
+        browser.findElement(By.xpath("//button")).click();
+
+        Assert.assertTrue(browser.getCurrentUrl().equals(URL_LOGIN));
+        Assert.assertTrue(browser.getPageSource().contains("Usuário e senha inválidos."));
+        Assert.assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")));
+
+    }
+
 }
